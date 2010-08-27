d6f04fff78d3d645cbc9fa0d8ebda460ab279615
Lincoln de Sousa <lincoln@comum.org>
2010-08-26 23:49:18 -0300

    Importando o request no projetos.py pra evitar um ImportError

--- cdpc/app/projetos.py
+++ cdpc/app/projetos.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from flask import Module, render_template
+from flask import Module, render_template, request
 
 from . import validators
 from . import models
