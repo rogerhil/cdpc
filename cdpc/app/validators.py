8285a2dd562f6130d4ddbb32785e8e3ba1fc45bc
Marco Túlio Gontijo e Silva <marcot@marcot.eti.br>
2010-08-27 11:21:05 -0300

    Campos string no validator do usuário

--- cdpc/app/validators.py
+++ cdpc/app/validators.py
@@ -51,8 +51,25 @@ class CpfValidator(formencode.FancyValidator):
         return value
 
 class Usuario(formencode.Schema):
+    voce_eh = validators.String(not_empty=True)
     nome = validators.String(not_empty=True)
     cpf = CpfValidator(not_empty=True)
+    data_nascimento = validators.String(not_empty=True)
+    sexo = validators.String(not_empty=True)
+    end_cep = validators.String(not_empty=True)
+    end_numero = validators.String(not_empty=True)
+    end_uf = validators.String(not_empty=True)
+    end_cidade = validators.String(not_empty=True)
+    end_bairro = validators.String(not_empty=True)
+    telefone = validatores.String(not_empty=True)
+    email = validators.String(not_empty=True)
+    usuario = validators.String(not_empty=True)
+    senha = validators.String(not_empty=True)
+    confirmar_senha = validators.String(not_empty=True)
+
+    # TODO:
+    #   Comparar senha original e confimada
+    #   Checar dados condicionais
 
 class Projeto(formencode.Schema):
     voce_eh = validators.String(not_empty=True)
