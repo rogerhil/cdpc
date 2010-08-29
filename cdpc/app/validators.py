942d10392bb3b79bdf5ccff8e9be0716910246e0
Marco Túlio Gontijo e Silva <marcot@marcot.eti.br>
2010-08-29 20:15:48 -0300

    Completa o validator do projeto.

--- cdpc/app/validators.py
+++ cdpc/app/validators.py
@@ -93,24 +93,190 @@ class Usuario(formencode.Schema):
     #   Checar dados condicionais
 
 class Projeto(formencode.Schema):
+    # -- Dados do projeto
     voce_eh = validators.String(not_empty=True)
     tipo_convenio = validators.String(not_empty=True)
     numero_convenio = validators.String(not_empty=True)
     nome_proj = validators.String(not_empty=True)
+
+    # -- Localização geográfica do projeto
     end_proj_cep = validators.String(not_empty=True)
     end_proj_numero = validators.String(not_empty=True)
+    end_proj_logradouro = validators.String(not_empty=True)
+    end_proj_complemento = validators.String()
     end_proj_uf = validators.String(not_empty=True)
     end_proj_cidade = validators.String(not_empty=True)
     end_proj_bairro = validators.String(not_empty=True)
+    end_proj_latitude = validators.String()
+    end_proj_longitude = validators.String()
     local_proj = validators.String(not_empty=True)
-    tel_proj = validators.String(not_empty=True)
-    email = validators.String(not_empty=True)
+    end_outro_nome = validators.String()
+    end_outro_cep = validators.String()
+    end_outro_numero = validators.String()
+    end_outro_logradouro = validators.String()
+    end_outro_complemento = validators.String()
+    end_outro_uf = validators.String()
+    end_outro_cidade = validators.String()
+    end_outro_bairro = validators.String()
+    end_outro_latitude = validators.String()
+    end_outro_longitude = validators.String()
+
+    # -- Contatos e espaços na rede
+    tel_proj = validators.String()
+    email_proj = validators.String(not_empty=True)
+    website_proj = validators.URL()
+    rs_nome = validators.String()
+    rs_link = validators.String()
+    feed_nome = validators.String()
+    feed_link = validators.String()
+
+    # -- Comunicação e Cultura Digital
     sede_possui_tel = validators.String(not_empty=True)
+    tipo_tel_sede = validators.String()
+    pq_sem_tel = validators.String()
+    pq_sem_tel_outro = validators.String()
     sede_possui_net = validators.String(not_empty=True)
+    tipo_internet = validators.String()
+    pq_sem_internet = validators.String()
+    pq_sem_internet_outro = validators.String()
+
+    # -- Entidade Proponente
     nome_ent = validators.String(not_empty=True)
     endereco_ent_proj = validators.String(not_empty=True)
+    end_ent_cep = validators.String()
+    end_ent_numero = validators.String()
+    end_ent_logradouro = validators.String()
+    end_ent_complemento = validators.String()
+    end_ent_uf = validators.String()
+    end_ent_cidade = validators.String()
+    end_ent_bairro = validators.String()
+    end_ent_latitude = validators.String()
+    end_ent_longitude = validators.String()
+    tel_ent = validators.String()
+    email_ent = validators.String()
+    website_ent = validators.URL()
+    convenio_ent = validators.String()
+    outro_ent = validators.String()
+
+    # -- Atividades exercidas pelo projeto
+    # -- Qual a área de atuação das atividades do projeto
+    cultura_popular = validators.Bool()
+    direitos_humanos = validators.Bool()
+    economia_solidaria = validators.Bool()
+    educacao = validators.Bool()
+    esportes_e_lazer = validators.Bool()
+    etnia = validators.Bool()
+    genero = validators.Bool()
+    habitacao = validators.Bool()
+    meio_ambiente = validators.Bool()
+    memoria = validators.Bool()
+    patrimonio_historico_imaterial = \
+        validators.Bool()
+    patrimonio_historico_material = \
+        validators.Bool()
+    pesquisa_e_extensao = validators.Bool()
+    povos_tradicionais = validators.Bool()
+    recreacao = validators.Bool()
+    religiao = validators.Bool()
+    saude = validators.Bool()
+    sexualidade = validators.Bool()
+    tecnologia = validators.Bool()
+    trabalho = validators.Bool()
+    outras_atividades = validators.Bool()
+    quais_outras_atividades = validators.String()
+
+    # ---  Com qual Público Alvo o Projeto é desenvolvido?
+    # ---- Sob aspectos de Faixa Etária
+    criancas = validators.Bool()
+    adolescentes = validators.Bool()
+    adultos = validators.Bool()
+    jovens = validators.Bool()
+
+    # ---- Sob aspectos das Culturas Tradicionais
+    quilombola = validators.Bool()
+    pomerano = validators.Bool()
+    caicara = validators.Bool()
+    indigena = validators.Bool()
+    cigana = validators.Bool()
+    povos_da_floresta = validators.Bool()
+    ribeirinhos = validators.Bool()
+    outras_culturas = validators.Bool()
+    quais_outras_culturas = validators.String()
+
+    # ---- Sob aspectos de Ocupação do Meio
+    rural = validators.Bool()
+    urbano = validators.Bool()
+    outro = validators.Bool()
+    outra_ocupacao = validators.Bool()
+    qual_outra_ocupacao = validators.String()
+
+    # ---- Sob aspectos de Gênero
+    mulheres = validators.Bool()
+    homens = validators.Bool()
+    lgbt = validators.Bool()
+
+    # --- Quais são as Manifestações e Linguagens que o Projeto utiliza
+    # em suas atividades?
+    artes_digitais = validators.Bool()
+    artes_plasticas = validators.Bool()
+    audiovisual = validators.Bool()
+    circo = validators.Bool()
+    culinaria = validators.Bool()
+    danca = validators.Bool()
+    fotografia = validators.Bool()
+    grafite = validators.Bool()
+    internet = validators.Bool()
+    jornalismo = validators.Bool()
+    literatura = validators.Bool()
+    musica = validators.Bool()
+    radio = validators.Bool()
+    teatro = validators.Bool()
+    tecnologias_digitais = validators.Bool()
+    tradicao_oral = validators.Bool()
+    tv = validators.Bool()
+    outras_manifestacoes = validators.Bool()
+    quais_outras_manifestacoes = validators.String()
+
+    # --- O Projeto participa de alguma Ação do Programa Cultura Viva?
     participa_cultura_viva = validators.String(not_empty=True)
+    agente_cultura_viva = validators.Bool()
+    cultura_digital = validators.Bool()
+    cultura_e_saude = validators.Bool()
+    economia_viva = validators.Bool()
+    escola_viva = validators.Bool()
+    grios = validators.Bool()
+    interacoes_esteticas = validators.Bool()
+    midias_livres = validators.Bool()
+    pontinho_de_cultura = validators.Bool()
+    pontos_de_memoria = validators.Bool()
+    redes_indigenas = validators.Bool()
+    tuxaua = validators.Bool()
+
+    descricao = validators.String()
+
+    # TODO: Tratar upload de documentacoes
+
+    # -- Parcerias do Projeto
     parcerias = validators.String(not_empty=True)
+    parc_biblioteca = validators.Bool()
+    parc_empresa = validators.Bool()
+    parc_equipamento_de_saude = validators.Bool()
+    parc_escola = validators.Bool()
+    parc_igreja = validators.Bool()
+    parc_ong = validators.Bool()
+    parc_poder_publico = validators.Bool()
+    parc_pontos_de_memoria = validators.Bool()
+    parc_redes_indigenas = validators.Bool()
+    parc_sistema_s = validators.Bool()
+    parc_tuxaua = validators.Bool()
+    outros_parceiros = validators.Bool()
+    quais_outros_parceiros = validators.String()
+    parc_nome = validators.String()
+
+    # -- Índice de acesso à cultura
+    ind_oficinas = validators.String()
+    ind_expectadores = validators.String()
+    ind_populacao = validators.String()
 
     # TODO:
     #   Validar endereços adicionados em Outros Locais e Entidade
