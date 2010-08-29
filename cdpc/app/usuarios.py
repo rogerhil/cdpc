a634416c8016d4dec6ad8f36f9513c28769033b8
Lincoln de Sousa <lincoln@comum.org>
2010-08-29 10:19:09 -0300

    Salvando os dados do cadastro de usuários no banco
    Esse commit não faz com que os dados sejam realmente salvos por
    causa de um bugzinho no validador. O campo de data de nascimento
    está sendo validado como uma string, ao invés de usar um DateTime.
    
    Assim q esse bug for corrigido, os dados já deveram ser salvos
    normalmente.

--- cdpc/app/usuarios.py
+++ cdpc/app/usuarios.py
@@ -21,6 +21,7 @@ from formencode import Invalid
 from urllib import urlopen
 from simplejson import dumps, loads
 from flask import Module, request, render_template
+from elixir import session
 
 from . import validators
 from . import models
@@ -36,19 +37,59 @@ def novo():
         # instanciando o validador
         validator = validators.Usuario()
         validado = {}
+        import pdb; pdb.set_trace()
         try:
             validado = validator.to_python(request.form)
+            clean_list = lambda x: [i for i in x if i.strip()];
+
+            rs_nomes = clean_list(request.form.getlist('rs_nome'))
+            rs_links = clean_list(request.form.getlist('rs_link'))
+            assert len(rs_nomes) == len(rs_links)
+
+            feed_nomes = clean_list(request.form.getlist('feed_nome'))
+            feed_links = clean_list(request.form.getlist('feed_link'))
+            assert len(feed_nomes) == len(feed_links)
+
         except Invalid, e:
             # Dar um feedback pro usuário usando a instância da
             # exceção "e".
+            pass
+        except AssertionError, e:
+            # Feedback sobre erros nos campos de lista
             pass
         else:
             # Instanciando o modelo e associando os campos validados e
             # transformados em valores python à instância que será
             # salva no db.
-            usuario = models.Usuario()
+
+            usuario = models.Pessoa()
+
+            # -- Dados pessoais
             usuario.nome = validado['nome']
             usuario.cpf = validado['cpf']
+            usuario.data_nascimento = validado['data_nascimento']
+            usuario.sexo = validado['sexo']
+
+            # -- Contatos e espaços na rede
+            usuario.email = validado['email']
+            usuario.website = validado['website']
+            for i in request.form.getlist('telefone'):
+                tel = models.Telefone()
+                tel.numero = i
+                usuario.telefones.append(tel)
+
+            for i in range(len(rs_nomes)):
+                rsocial = models.RedeSocial()
+                rsocial.nome = rs_nomes[i]
+                rsocial.link = rs_links[i]
+                usuario.redes_sociais.append(rsocial)
+
+            for i in range(len(feed_nomes)):
+                feed = models.Feed()
+                feed.nome = feed_nomes[i]
+                feed.link = feed_links[i]
+                usuario.feeds.append(feed)
+
             session.commit()
 
             # FIXME: Avisar ao usuário que tudo deu certo.
